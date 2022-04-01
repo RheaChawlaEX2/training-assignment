32b16cf5bd07c92f78a3e0c1626001a078637496
Rhea Chawla <rchawla@ex2india.com>
2022-04-01 12:10:39 +0530

    Commit

--- index.js
+++ index.js
@@ -1,40 +1,85 @@
-function add_task(){
-    var task = document.querySelector("#todo_task").value;
-    var desc = document.querySelector("#todo_desc").value;
-    var date = document.querySelector("#todo_date").value;
+export class Task{
+    constructor(task,desc, date){
+        this.task = task;
+        this.desc = desc;
+        this.date = date;
+    }
+     add_new_task(task, desc, date){
+        var list = document.querySelector('ul');
+    
+        const listItem = document.createElement('li');
+        const listText = document.createElement('span');
+        const listDeleteBtn = document.createElement('button');
+        const listEditBtn = document.createElement('button');
+    
+        listItem.appendChild(listText);
+        listText.textContent = `${task} : ${desc} : ${date}`;
+    
+        listItem.appendChild(listEditBtn);
+        listDeleteBtn.textContent = "Delete";
+    
+        listItem.appendChild(listDeleteBtn);
+        listEditBtn.textContent = "Edit";
+    
+        list.appendChild(listItem);
+    
+        listDeleteBtn.addEventListener('click', () => {
+            list.removeChild(listItem);
+        });
+    
+        listEditBtn.onclick = function(){ 
+            var editObj = new Edit(list,listItem,listDeleteBtn,listEditBtn,task,desc,date);
+            editObj.edit_event(list, listItem, listText, listDeleteBtn, listEditBtn, task, desc, date); }
+    
+    }
+   // update_edit_event_listener(){}   
 
-    add_new_task(task, desc, date);
-}
+   edit_event(list, listItem, listText, listDeleteBtn, listEditBtn, task, desc, date){
 
-function add_new_task(task, desc, date){
-    var list = document.querySelector('ul');
+    listItem.removeChild(listEditBtn);
+    listItem.removeChild(listDeleteBtn);
 
-    const listItem = document.createElement('li');
-    const listText = document.createElement('span');
-    const listDeleteBtn = document.createElement('button');
-    const listEditBtn = document.createElement('button');
+    var new_task = document.createElement("input");
+    new_task.type = "text";
+    new_task.value = task;
 
-    listItem.appendChild(listText);
-    listText.textContent = `${task} : ${desc} : ${date}`;
+    var new_desc = document.createElement("input");
+    new_desc.type = "text";
+    new_desc.value = desc;
 
-    listItem.appendChild(listEditBtn);
-    listDeleteBtn.textContent = "Delete";
+    var new_date = document.createElement("input");
+    new_date.type = "date";
+    new_date.value = date;
 
+    var listAddBtn = document.createElement('button');
+    listAddBtn.textContent = "Add";
+
+    listItem.replaceChild(new_task, listText);
+    listItem.appendChild(new_desc);
+    listItem.appendChild(new_date);
+    listItem.appendChild(listAddBtn);
     listItem.appendChild(listDeleteBtn);
-    listEditBtn.textContent = "Edit";
 
-    list.appendChild(listItem);
-
-    listDeleteBtn.addEventListener('click', () => {
-        list.removeChild(listItem);
+    listAddBtn.addEventListener('click', () => {
+        listText.textContent = `${new_task.value} : ${new_desc.value} : ${new_date.value}`;
+        listItem.replaceChild(listText, new_task);
+        listItem.replaceChild(listEditBtn, listAddBtn);
+        listItem.removeChild(new_desc);
+        listItem.removeChild(new_date);
     });
 
-    listEditBtn.onclick = function(){ edit_event(list, listItem, listText, listDeleteBtn, listEditBtn, task, desc, date); }
-
+    listEditBtn.onclick = function() { edit_event(list, listItem, listText, listDeleteBtn, listEditBtn, new_task.value, new_desc.value, new_date.value); }
+    
+   
+}
 }
 
-function update_edit_event_listener(){}
 
+
+
+
+
+/*
 function edit_event(list, listItem, listText, listDeleteBtn, listEditBtn, task, desc, date){
 
     listItem.removeChild(listEditBtn);
@@ -70,4 +115,4 @@ function edit_event(list, listItem, listText, listDeleteBtn, listEditBtn, task, 
     });
 
     listEditBtn.onclick = function() { edit_event(list, listItem, listText, listDeleteBtn, listEditBtn, new_task.value, new_desc.value, new_date.value); }
-}
+}*/
