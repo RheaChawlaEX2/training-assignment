0f7596677635eb1482ae172ffc63fd75e5c680e9
Rhea Chawla <rchawla@ex2india.com>
2022-04-04 11:32:38 +0530

    LocalStorage Adding Multiple

--- dom.js
+++ dom.js
@@ -5,8 +5,13 @@ class Task{
         this.task = task;
         this.desc = desc;
         this.date = date;
+        
     }
      add_new_task(task, desc, date){
+       // let addObj = new AddStorage(task, desc, date);
+
+    //    let extract = JSON.parse(localStorage.getItem('tasks'));
+    //     console.log(extract);
         var list = document.querySelector('ul');
     
         const listItem = document.createElement('li');
@@ -80,41 +85,49 @@ class EditTask extends Task {
             listItem.replaceChild(listEditBtn, listAddBtn);
             listItem.removeChild(new_desc);
             listItem.removeChild(new_date);
-
-            // var data = { 
-            //     "taskData": new_task.value,
-            //     "descData": new_desc.value,
-            //     "dateData" : new_date.value
-            // };
-            // localStorage.setItem("data", JSON.stringify(data));
-            // var allTasks = new Array();
-            // allTasks.push(task);
-            // localStorage.setItem("allTasks", JSON.stringify(allTasks));        
-            // let obj = new Task(task, desc, date);
         });
     
         listEditBtn.onclick = function() { edit_event(list, listItem, listText, listDeleteBtn, listEditBtn, new_task.value, new_desc.value, new_date.value); }
     }
 }
-    
+  
+var id = 0;
+var counter = 0;
 
-
-  function add_task(){
-
-        var task = document.querySelector("#todo_task").value;
-        var desc = document.querySelector("#todo_desc").value;
-        var date = document.querySelector("#todo_date").value;
+class AddStorage extends Task{
+    constructor(task, desc, date){
+        super(task, desc, date);
+        // this.task = task;
+        // this.date = date;
+        // this.desc = desc;
+    }
+    add_to_localStorage(task, desc, date){
         var data = { 
             "taskData": task,
             "descData": desc,
             "dateData" : date
         };
-        var allTasks = new Array();
-        data.allTasks = allTasks;   
-        localStorage.setItem("data", JSON.stringify(data));
-        
-        //       
-        let obj = new Task(task, desc, date);
+
+        var items = new Object();
+     
+           items =  Object.assign(counter++,data );
+           console.log(items);       
+        localStorage.setItem(`${id++}`, JSON.stringify(data)); 
+     
+    }
+}
+
+
+  function add_task(){
     
+       
+        var task = document.querySelector("#todo_task").value;
+        var desc = document.querySelector("#todo_desc").value;
+        var date = document.querySelector("#todo_date").value;      
+        let obj = new Task(task, desc, date);
         obj.add_new_task(task, desc, date);
+
+        let local = new AddStorage(task, desc, date);
+        local.add_to_localStorage(task, desc, date);
+         
     }
