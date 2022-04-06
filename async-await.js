2e75954dc6398016484ca31e64ca027ca862367e
RheaChawlaEX2 <rchawla@ex2india.com>
2022-04-06 21:19:05 +0530

    Promises and Aync-Await

---
+++ async-await.js
@@ -0,0 +1,42 @@
+let today = new Date()
+/*-------Using Async-Await-----*/
+async function isWeekend(today){
+    let weekendCheck = new Promise((resolve,reject) => {
+        setTimeout(() => {
+            console.log("/*-------Using Async Await-----*/")
+            if(today.getDay() == 6 || today.getDay() == 0){
+                resolve();
+            }
+            else{
+            let weekend = (today.getDay()>0) ? (6 - today.getDay()) : (0 - today.getDay());
+            reject(weekend);
+                reject()
+            }
+           
+            
+        },2000);
+    }) 
+    await weekendCheck;
+  
+}
+
+
+
+isWeekend(today).then((resolve) =>{
+    resolve('true');
+} ).catch((weekend) => 
+console.log(`Today, is day ${today.getDay()} and Weekend is ${weekend} of days left`)
+)
+.finally(() => {
+    switch(today.getDay()){
+        case 0: console.log('Sunday'); break;
+        case 1: console.log('Monday'); break;
+        case 2: console.log('Tuesday'); break;
+        case 3: console.log('Wednesday'); break;
+        case 4: console.log('Thursday'); break;
+        case 5: console.log('Friday'); break;
+        case 6: console.log('Saturday'); break;
+        default: console.log('error')
+    }
+  
+});
